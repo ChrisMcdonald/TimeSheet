f36305b09f563b139d26530c27ef1fbaf1425765
ChrisMcDonald <chris78323@gmail.com>
2017-08-27 13:04:38 +1000

    add list of projects to the project dropdown box

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,12 @@
 class Project < ApplicationRecord
 	# has_many :invoices
 
+	def self.select_attributes
+		result = Array.new
+		Project.all.select(:id,:name).each do |p|
 
+			result.append([p.name,p.id])
+		end
+		result
+	end
 end
