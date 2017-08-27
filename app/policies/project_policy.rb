e2c6e830e34d47cf6f7c62fa72bd034ef4899f5d
ChrisMcDonald <chris78323@gmail.com>
2017-08-27 14:08:42 +1000

    fix update project

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -2,16 +2,14 @@ class ProjectPolicy < ApplicationPolicy
 
   attr_reader :user, :project
   class Scope < Scope
-    def initialize(user, prokect)
+    def initialize(user, project)
       @user = user
       @project = project
     end
     def destroy?
-      @project.user == 1
     end
 
     def update?
-      @project.user == 1r
     end
   end
 end
