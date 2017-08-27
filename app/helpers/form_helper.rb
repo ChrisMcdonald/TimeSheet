63ac66d3c9a57d78433f328af8e8cd90f3bbf45a
ChrisMcDonald <chris78323@gmail.com>
2017-08-27 13:14:55 +1000

    basic form with muiltiple invoice_rows

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,7 +1,7 @@
 module FormHelper
 	def setup_invoice(invoice)
-		invoice.invoice_rows.build ||= InvoiceRow.new
-		# 1.times { invoice.invoice_rows.build }
+		invoice.invoice_rows||= InvoiceRow.new
+		3.times { invoice.invoice_rows.build }
 		invoice
 	end
 end
