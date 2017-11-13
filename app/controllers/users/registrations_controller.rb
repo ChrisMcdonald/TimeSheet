472da6191c344b9965209529ae329c177a4369f9
ChrisMcDonald <chris78323@gmail.com>
2017-11-14 06:39:05 +1000

    registed user to be admin

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,9 +9,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	# end
 
 	# POST /resource
-	# def create
-	#   super
-	# end
+	def create
+		super
+		resource.add_role :admin
+	end
 
 	# GET /resource/edit
 	# def edit
