162e44c57be43959d3cfbe44374776b3990fa756
ChrisMcDonald <chris78323@gmail.com>
2017-12-01 18:06:50 +1000

    fix devise redirect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,30 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # helper_method :current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :user_not_authorized
 
-  rescue_from CanCan::AccessDenied do |exception|
-	  redirect_to root_url, :alert => exception.message
-  end
+
+  rescue_from CanCan::AccessDenied, with: :user_not_authorized
   private
+
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-	redirect_to(redirect_to new_user_registration_url || edit_project_path) unless user_signed_in?
+    redirect_to user_session_path
   end
+
   def set_time_zone
 	  Time.zone = "Australia/Brisbane"
   end
   protected
+
+  def authenticate_user!
+
+    redirect_to user_session_path, :notice => 'if you want to add a notice'
+    ## if you want render 404 page
+    ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+
+  end
+
   def configure_permitted_parameters
 
 	  devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :first_name, :last_name, :street_no, :street, :city, :state,
