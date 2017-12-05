1e9a4f84580e8144a3f1208c58384b33a077ccb7
ChrisMcDonald <chris78323@gmail.com>
2017-12-05 15:47:43 +1000

    social media login

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
 	devise_for :users, path: :admin,
 			   controllers: {#registrations: "users/registrations",
 				 sessions: 'users/sessions',
-				 omniauth_callbacks: 'users/omniauth_callbacks'
+				 omniauth_callbacks: 'omniauth_callbacks'
          			   }
 
 	resources :users do
