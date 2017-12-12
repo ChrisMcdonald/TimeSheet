0d729e6a399f3de58e93ec74f07f0c3cd17da470
ChrisMcDonald <chris78323@gmail.com>
2017-12-12 22:34:30 +1000

    chat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,9 +37,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://earth-broken.bnr.la/cable'
-  config.action_cable.allowed_request_origins = [ 'http://earth-broken.bnr.la' ]
+  # config.action_cable.mount_path = '/cable'
+  # config.action_cable.url = 'wss://earth-broken.bnr.la/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://earth-broken.bnr.la' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
