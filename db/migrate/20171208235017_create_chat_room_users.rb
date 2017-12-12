4abc9344cb28d4578018f3d9693a4c1d11b97ec5
ChrisMcDonald <chris78323@gmail.com>
2017-12-12 20:18:18 +1000

    chat

--- db/migrate/20171208235017_create_chat_room_users.rb
+++ db/migrate/20171208235017_create_chat_room_users.rb
@@ -1,7 +1,7 @@
 class CreateChatRoomUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :chatroom_users do |t|
-      t.references :chatrooms, foreign_key: true
+      t.references :chatroom, foreign_key: true
       t.references :user, foreign_key: true
 
       t.timestamps
