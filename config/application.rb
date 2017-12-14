8f081e80062d3f96d8e2d75d3de9db8b1d3921a4
ChrisMcDonald <chris78323@gmail.com>
2017-12-15 06:13:16 +1000

    chat

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Devise3
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Australia/Brisbane'
+    config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
 
   end
 end
